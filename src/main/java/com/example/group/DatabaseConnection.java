96c57cb105d1025e2dde3d3e43fb15fdff7956c6
Emmanuel Yeboah <76918241+IchaCoder@users.noreply.github.com>
2023-07-26 08:09:07 +0000

    Update DatabaseConnection.java

--- src/main/java/com/example/group/DatabaseConnection.java
+++ src/main/java/com/example/group/DatabaseConnection.java
@@ -7,7 +7,7 @@ public class DatabaseConnection {
     public Connection databaseLink;
 
     public Connection getConnection () {
-        String databaseName = "pharmacy";
+        String databaseName = "store";
         String databaseUser = "root";
         String databasePassword = "oracle";
         String url = "jdbc:mysql://localhost:3306/" + databaseName;
